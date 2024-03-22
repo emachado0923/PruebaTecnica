f44fd19c9ecb63e9eded5cc557222433754f3864
Emerson Machado <emachado0923@gmail.com>
2024-03-21 19:39:36 -0500

    Configuracion Mongo URL

--- src/utils/mongoose.ts
+++ src/utils/mongoose.ts
@@ -1,6 +1,6 @@
 const mongoose=require("mongoose");
 
 mongoose
-.connect("mongodb://127.0.0.1/productoCRUD")
+.connect("mongodb+srv://emachado0923:Rh5dwZoKjzyawcMq@cluster0.6ekpxfe.mongodb.net/")
 .then(()=>console.log("MongoDB Connected..."))
 .catch((err: any)=>console.log(err));
